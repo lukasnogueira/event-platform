87ce9d2ec6a599dfd9c3ba48b6367123abe4c39d
lukasnogueira <79331207+lukasnogueira@users.noreply.github.com>
2023-05-24 08:38:03 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  
+  return (
+    <>
+      <p className=" text-5xl font-bold text-violet-500">Hello Word!!</p>
+    </>
+  )
+}
+
+export default App
+
+//CMS = Content Manangerment System
+
+//        OLD
+//wordPress = Traz o painem ADMIN é o font-end (Temas)
+// =====================================================================
+
+//         NEW
+//Handless CMS (GraphCMS) = Painel ADMIN (dados dornecidos atravez de API(Rest ou graphQL))
+
+// React consome essa API do CMS 
